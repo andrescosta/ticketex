1115be3263564b29113711362a4d057024227aea
Andres <andres.costa@gmail.com>
2023-06-01 15:57:06 -0400

    updates

--- data/common/tiklib/repository/entity_repository.py
+++ data/common/tiklib/repository/entity_repository.py
@@ -1,13 +1,8 @@
-from abc import ABC, abstractproperty
-from ast import Dict
-from types import SimpleNamespace
 from typing import Any
-from motor.motor_asyncio import AsyncIOMotorClient
-from pymongo.database import Database
 from pymongo.collection import Collection
 import copy
 from bson import ObjectId
-from tiklib.common import app_settings
+from tiklib.db.client import get_db
 
 
 
@@ -16,14 +11,10 @@ from tiklib.repository.entity_not_found_error import EntityNotFoundError
 
 
 class EntityRepository:
-    __client:AsyncIOMotorClient = AsyncIOMotorClient(app_settings.DATABASE_URL)
-    __db:Database = __client[app_settings.DATABASE_NAME]
-    __collection:Collection = None
-
-    def __init__(self, entity_type:type) -> None:
+ 
+    def __init__(self,entity_type:type) -> None:
         self.entity_type=entity_type
-        self.__collection=self.__db[self.collection_name(entity_type)]
-
+        
     async def save(self, entity:Entity)->Entity:
         entity_dict = self.for_saving(entity)
         result = await self.collection().insert_one(entity_dict)
@@ -56,7 +47,7 @@ class EntityRepository:
         return f"{entitytype.__name__.lower()}s"
     
     def collection(self)->Collection:
-        return self.__collection
+        return get_db()[self.collection_name(self.entity_type)]
 
     def for_saving(self, entity:Entity)->dict[str, Any]:
         entity_dict = entity.dict(exclude_unset=True)
